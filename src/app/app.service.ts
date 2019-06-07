90044a5a7a105e1f891df8d88616722e3e340520
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-06-07 15:46:05 -0400

    added boolean value to see voting is done

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,15 +6,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppService {
 
-  constructor(private http: HttpClient) {}
+  public showPolls:boolean = true;
+
+  constructor(private http: HttpClient ) {}
   
   getDetails():any{
-    return this.http.get('http://voting-app-11e11.firebaseio.com/data1.json')
+    return this.http.get('https://voting-app-11e11.firebaseio.com/data1.json')
   }
+
   getMemberDetails():any{
-    return this.http.get('http://voting-app-11e11.firebaseio.com/data2.json')
+    return this.http.get('https://voting-app-11e11.firebaseio.com/data2.json')
   }
-  addVoteCount(data):any{
-    return this.http.post('http://voting-app-11e11.firebaseio.com/data2.json',data)
+
+  addVoteCount(data, key):any{
+   this.http.put(`https://voting-app-11e11.firebaseio.com/data2/${key}.json`, data)
+      .subscribe(
+        res => console.log("Put Request is Succesfull"),
+        error => console.log('Error', error)
+      )
+  }
+  
+  showPollChart(){
+    return this.showPolls = false;
   }
 }
+ 
