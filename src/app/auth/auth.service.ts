6dbd9085e0c43e074cee127ee1b709fec8e0aad8
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-06-10 16:49:21 -0400

    added auth with google and facebook/setup authguards

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Router } from  '@angular/router';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import  * as firebase from  'firebase/app';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private user: Observable<firebase.User>;
+  private userDetails: firebase.User = null;
+
+
+  constructor( private firebaseAuth: AngularFireAuth, private router: Router) { 
+    this.user = firebaseAuth.authState;
+    this.user.subscribe(
+      user => {
+        if(user){
+          this.userDetails = user;
+          console.log(this.userDetails);
+        }else{
+          this.userDetails = null;
+        }
+      }
+    );
+  }
+
+  signInWithGoogle(){
+    return this.firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.GoogleAuthProvider()
+    )
+  }
+
+  signInWithFacebook(){
+    return this.firebaseAuth.auth.signInWithPopup(
+      new firebase.auth.FacebookAuthProvider()
+    )
+  }
+  
+  
+  isLoggedIn() {
+    if (this.userDetails === null) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  logout() {
+    this.firebaseAuth.auth.signOut()
+    .then((res) => this.router.navigate(['/']));
+  }
 }
