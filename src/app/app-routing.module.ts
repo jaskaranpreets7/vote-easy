82a564c0e4033f57f0c8b6d70a92904da5274b3b
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-06-10 16:52:30 -0400

    used authguard on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { PollDetailComponent } from './poll-detail/poll-detail.component';
 import { PollListComponent } from './poll-list/poll-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AuthGuardService } from './auth/auth-guard.service';
+
 
 const routes: Routes = [
-  { path : '', component: HomeComponent},
-  { path : 'home', component: HomeComponent },
-  { path : 'polls', component: PollListComponent },
-  { path : 'sign-in', component: SignInComponent},
-  { path : 'poll-details/:id', component: PollDetailComponent },
+  { path : '', component: SignInComponent},
+  { path : 'home', component: HomeComponent , canActivate:[AuthGuardService] },
+  { path : 'polls', component: PollListComponent, canActivate:[AuthGuardService]  },
+  { path : 'poll-details/:id', component: PollDetailComponent, canActivate:[AuthGuardService]  },
   { path : '**', component: NotFoundComponent}
 ];
 
