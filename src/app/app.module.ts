23b6c8ed09e66de9094ab7bd264ad298ebccb4bd
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-06-10 16:51:43 -0400

    imported angularAuth modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth'
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PollListComponent } from './poll-list/poll-list.component';
 import { PollDetailComponent } from './poll-detail/poll-detail.component';
-import { AppRoutingModule } from './app-routing.module';
 import { AppService } from './app.service';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { ChartComponent } from './chart/chart.component';
+import { AuthService } from './auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -28,8 +34,11 @@ import { ChartComponent } from './chart/chart.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig ,'angular-auth-firebase'),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
-  providers: [AppService],
+  providers: [AppService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
